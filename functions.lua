c2ccad4b0e75d247ddbe3cc6f3c2e614a24c5e8c
cmentzer <cwolfmentzer@gmail.com>
2015-12-11 02:07:38 -0500

    made some progress, moved things around and created a main function, will do more later tonight

--- functions.lua
+++ functions.lua
@@ -1,5 +1,6 @@
 function test()
 	crawl.mpr("testing")
+end
 
 local function populate_item_list(x, y)
   floor_items = items.get_items_at(x, y)
@@ -50,11 +51,18 @@ end
 
 function get_game_state()
 	crawl.mpr("getting game state ...")
+	-- list of visible walls
 	local wall_list = {}
+	-- list of visible monsters
 	local monster_list = {}
+	-- list of visible water
 	local water_list = {}
+	-- list of visible lava
 	local lava_list = {}
-	item_list = {}
+
+
+	-- player is a variable that holds information about the player
+	local player = {you.xl(), you.hp(), you.hunger()}
 
 	-- for every cell inside our max vision range ... 
 	for x = -8,8 do
@@ -95,28 +103,31 @@ function get_game_state()
 	-- to print functions that display the current values in lists at the time of the button press in game. 
 
 	for key, value in pairs(wall_list) do 
-		crawl.mpr("there is a wall at " .. value[1] .. value[2])
+		--crawl.mpr("there is a wall at " .. value[1] .. value[2])
 	end
 
 	for key, value in pairs(monster_list) do 
-		crawl.mpr("There is a(n) " .. value [1] .. " at " .. value[2] .. value[3])
+		crawl.mpr("There is a(n) " .. value [1] .. " at " .. value[2] .. ", " .. value[3])
 	end 
 	for key, value in pairs(water_list) do
-		crawl.mpr("there is " .. value[1] .. " water at " .. value[2] .. value[3])
+	--	crawl.mpr("there is " .. value[1] .. " water at " .. value[2] .. value[3])
 	end
 	for key, value in pairs(lava_list) do
-		crawl.mpr("there is lava at " .. value[1] .. value[2])
+	--	crawl.mpr("there is lava at " .. value[1] .. value[2])
 	end
 	for key, value in pairs(item_list) do
 		 crawl.mpr("there is an item of class " .. value[1] .. " and name \"" .. value[2] .. "\" at position " .. value[3] .. ", " .. value[4])
 	end
 
-	crawl.mpr("with " .. table.getn(wall_list) .. " walls ... \n")
-	crawl.mpr(table.getn(monster_list) .. " monsters ... \n")
-	crawl.mpr(table.getn(lava_list) .. " tiles of lava ... \n")
-	crawl.mpr(table.getn(water_list) .. " tiles of water ... \n")
-	crawl.mpr("and " .. table.getn(item_list) .. " items on the floor ... \n")
-	crawl.mpr(" ... the current value attributed to this game state is: " .. "10 \n")
+	--crawl.mpr("with " .. table.getn(wall_list) .. " walls ... \n")
+	--crawl.mpr(table.getn(monster_list) .. " monsters ... \n")
+	--crawl.mpr(table.getn(lava_list) .. " tiles of lava ... \n")
+	--crawl.mpr(table.getn(water_list) .. " tiles of water ... \n")
+	--crawl.mpr("and " .. table.getn(item_list) .. " items on the floor ... \n")
+	--crawl.mpr(" ... the current value attributed to this game state is: " .. "10 \n")
+
+	local gameState = {wall_list, monster_list, water_list, lava_list, item_list}
+	return gameState
 
 end
 
@@ -132,7 +143,7 @@ function getAction(gameState)
 	for k,v in actionScores do
 		crawl.mpr(k)
 		crawl.mpr(v)
-
+	end
 	local maxScore = 0
 	local action = ""
 	for k,v in actionScores do
@@ -162,6 +173,7 @@ function maxValue(gameState, depth, agentIndex, action)
 		if v < potential then
 			v = potential
 		end
+	end
 	return v
 end
 
@@ -181,11 +193,97 @@ function minValue(gameState, depth, agentIndex, action)
 		if v < potential then
 			v = potential
 		end
+	end
 	return v
 end
 
 function scoreGameState(gameState)
-	-- TODO: DEFINE THIS. Cannot run without this being filled out.
+	-- first, lets get the score from the number of adjacent walls
+	local function numAdjWalls(wall_list)
+	local adjWalls = 0
+		for key, value in pairs(wall_list) do 
+			if ((value[1] == -1 or value[1] == 0 or value[1] == 1) and
+				(value[2] == -1 or value[2] == 0 or value[2] == 1)) then
+				-- this wall is adjacent to us, +1
+				adjWalls = adjWalls + 1
+			end
+		end 
+		return adjWalls
+	end
+
+	-- then, compute the score we get from the locations of monsters on the screen
+	local function getMonsterScore(monster_list)
+		-- undefined, for now just return number of monsters in the list
+		return table.getn(monster_list)
+	end
+
+	-- then, computer score from water. Like walls and lava, standing NEXT TO water is good, 
+	-- because mosnters standing in water get a debuff to hit chance. However we want to avoid 
+	-- STANDING in water, to avoid the debuff ourselves. Also like lava, standing IN DEEP water
+	-- kills you, so we dont want that
+	local function getWaterScore(water_list)
+		local adjWater = 0
+		local standingInSWater = false
+		local standingInDWater = false
+		for key, value in pairs(water_list) do
+			if ((value[2] == -1 or value[2] == 0 or value[2] == 1) and
+				(value[3] == -1 or value[3] == 0 or value[3] == 1)) then
+				-- this water is adjacent to us, +1
+				-- check if the adjacent water is shallow or deep, 
+				-- standing next to deep water is better
+				if value[1] == "shallow" then
+					adjWater = adjWater + 1
+				else 
+					adjWater = adjWater + 2
+				end
+			end
+			if value[2] == 0 and value[3] == 0 then
+				if value[1] == "shallow" then
+					standingInSWater = true
+				else 
+					standingInDWater = true
+				end
+			end
+		end 
+		if standingInSWater then
+			-- sometimes standing in shallow water is unavoidable, so we want to make sure
+			-- that the value we attribute to it is not TOO low. 
+			return -5
+		elseif standingInDWater then
+			return -100000
+		else
+			return adjWater
+		end
+	end 
+
+	-- then, computer score from lava. For most purposes, we can consider lava to be the same
+	-- as walls (if we stand next to lava, there can't be a monster there). However, we need to
+	-- make sure we assign an extremely negative value to STANDING in lava. 
+	local function getLavaScore(lava_list)
+		local adjLava = 0
+		local standingInLava = false
+		for key, value in pairs(lava_list) do 
+			if ((value[1] == -1 or value[1] == 0 or value[1] == 1) and
+				(value[2] == -1 or value[2] == 0 or value[2] == 1)) then
+				-- this lava is adjacent to us, +1
+				adjLava = adjLava + 1
+			end
+			if value[1] == 0 and value[2] == 0 then
+				standingInLava = true
+			end
+		end 
+		if standingInLava then
+			return -100000
+		else
+			return adjLava
+		end
+	end
+	-- now computer the total score of the given gamestate
+	local wallScore = numAdjWalls(gameState[1])
+	local monsterScore = getMonsterScore(gameState[2])
+	local waterScore = getWaterScore(gameState[3])
+	local lavaScore = getLavaScore(gameState[4])
+	return wallScore + monsterScore + lavaScore + waterScore
 end
 
 function isWinState(gameState)
@@ -196,10 +294,59 @@ function isLoseState(gameState)
 	-- undefined, probably forever
 end
 
-function getLegalActions(gameState)
-	--undefined
+function getLegalActions(agentIndex, gameState)
+	-- for the given agentIndex and gameState, get a list of the legal
+	-- actions of that agent.
+	local agentPosn = 
+	-- starting with movement: 
+	function getLegalMovementActions(agentIndex, gameState)
+		-- possible movement actions are one of: 
+		-- y, u, h, j, k, l, b, n. 
+		local adjTiles = {}
+		for x = -1,1 do
+			for y = -1,1 do
+				for key, value in pairs(gameState[1])
+				table.insert(adjTiles, {x,y})
+			end
+		end
+
+
+
 end
 
 function generateSuccessor(agentIndex, action)
 	-- undefined
+end
+
+
+function main()
+	-- Essentially we want to split the game state into two categories:
+	-- one is "combat", for which we score our gamestates based on things like
+		-- location of monsters, our location relative to monsters and environmental 
+		-- features, how much health we have, etc. 
+	-- the other is "non-combat", during which we handle exploration, hunger, 
+	-- equipment, managing skills, etc.
+
+	local inCombat = false
+	local gameState = get_game_state()
+	-- if there are monsters on the screen, we are in combat, and need to switch to 
+	-- our "combat" logic
+	if table.getn(gameState[2]) > 0 then
+		inCombat = true
+		agents = 1 + table.getn(gameState[2])
+	else 
+		inCombat = false
+	end
+	-- if we are in combat, this code will determine our actions:
+	if inCombat then
+		crawl.mpr("in combat !!!")
+		currentScore = scoreGameState(gameState)
+		crawl.mpr("IN MAIN the game state is equal to " .. currentScore)
+		-- list the actions available to the player:
+		crawl.mpr("the actions available to the player are: ")
+
+	-- if we are not in combat, this code will determine our actions:
+	else
+		crawl.mpr("not in combat")
+	end
 end
