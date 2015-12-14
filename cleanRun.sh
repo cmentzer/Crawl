90fa1af3f3a8308c4417160ba9db6ac93279cefb
alex jacks <alexjacks@comcast.net>
2015-12-14 16:15:11 -0500

    Added documentation and script instructions.

--- cleanRun.sh
+++ cleanRun.sh
@@ -1,3 +1,9 @@
+
+
+# this assumes you have a folder called "repo" inside your crawl installation
+# and then this repo cloned into that folder. Change this based on your file layout.
+# all it does it move in the functions.lua file and then kick off the game.
+# run this script from the DCSS installation folder.
 rm ./settings/functions.lua
 cp ./repo/Crawl/functions.lua ./settings/functions.lua
 ./crawl.exe
